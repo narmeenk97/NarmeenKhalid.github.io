e07e9cfe7976ffe3af2c400fb2b2078017f47b9a
narmeenk97 <144829367+narmeenk97@users.noreply.github.com>
2023-11-23 14:19:53 -0500

    Lab4-part2-commit4
    adding the alt descriptions for the images

--- Lab4/Part2/gallery-start/main.js
+++ Lab4/Part2/gallery-start/main.js
@@ -15,7 +15,13 @@ const overlay = document.querySelector('.overlay');
 const imagenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
-
+const altdescriptions = {
+    'pic1.jpg' : 'Closeup of a human eye',
+    'pic2.jpg' : 'Rock that looks like a wave',
+    'pic3.jpg' : 'Purple and white pansies',
+    'pic4.jpg' : 'Section of wall from an Egyptian tomb',
+    'pic5.jpg' : 'Large moth on a leaf'
+}
 
 /* Looping through images */
 
