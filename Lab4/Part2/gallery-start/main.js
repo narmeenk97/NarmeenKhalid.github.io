08014b53431116b8f30e349637403f00bbeb7b00
narmeenk97 <144829367+narmeenk97@users.noreply.github.com>
2023-11-23 14:15:27 -0500

    Lab4-Part2-Commit3
    Adding the const for image names

--- Lab4/Part2/gallery-start/main.js
+++ Lab4/Part2/gallery-start/main.js
@@ -12,8 +12,10 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imagenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
+
 
 /* Looping through images */
 
