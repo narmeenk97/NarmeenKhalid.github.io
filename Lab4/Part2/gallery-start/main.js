ec7e72a2e2d212bec83bb75a592900b67dcd4121
narmeenk97 <144829367+narmeenk97@users.noreply.github.com>
2023-11-23 21:50:46 -0500

    lab4-part2-commit6
    last commit - fixed the src for the images

--- Lab4/Part2/gallery-start/main.js
+++ Lab4/Part2/gallery-start/main.js
@@ -12,7 +12,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imagenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
+const images = ["pic1.jpg", `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`]
 
 /* Declaring the alternative text for each image file */
 const altdescriptions = {
@@ -25,10 +25,10 @@ const altdescriptions = {
 
 /* Looping through images */
 
-for (const image of imagenames) {
+for (const image of images) {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', `imagenames/${image}`);
-    newImage.setAttribute('alt', `altdescriptions[image]`);
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', altdescriptions[image]);
     thumbBar.appendChild(newImage);
     newImage.addEventListener('click', e => {
         displayedImage.src = e.target.src;
@@ -37,3 +37,17 @@ for (const image of imagenames) {
 }
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+})
+
