a6df5b70a9f6d311661b2d415b149f970e012c96
narmeenk97 <144829367+narmeenk97@users.noreply.github.com>
2023-11-24 13:42:56 -0500

    lab4-part3-commit
    corrected some syntax errors

--- Lab4/Part3/main.js
+++ Lab4/Part3/main.js
@@ -67,13 +67,14 @@ class Ball {
 
 	collisionDetect() {
 		for (const ball of balls) {
-			if (this !== ball) {
+			if (!(this === ball)) {
 				const dx = this.x - ball.x;
 				const dy = this.y - ball.y;
 				const distance = Math.sqrt(dx * dx + dy * dy);
 
-				if (distance < this.size + ball.size);
-				ball.color = this.color = randomRGB();
+				if (distance < this.size + ball.size){
+					ball.color = this.color = randomRGB();
+				}
 			}
 		}
 	}
@@ -98,7 +99,7 @@ while (balls.length < 25) {
 }
 
 function loop() {
-	ctx.fillstyle = "rgba(0, 0, 0, 0.25)";
+	ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
 	ctx.fillRect(0, 0, width, height);
 
 	for (const ball of balls) {
