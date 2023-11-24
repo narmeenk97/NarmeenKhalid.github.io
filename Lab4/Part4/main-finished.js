ceb963ed2ce05034a1ec0ab3c00961e68daddf93
narmeenk97 <144829367+narmeenk97@users.noreply.github.com>
2023-11-23 22:32:35 -0500

    lab4-part4-commit4
    adding evilcircle class and defining the constructor

--- Lab4/Part4/main-finished.js
+++ Lab4/Part4/main-finished.js
@@ -86,6 +86,15 @@ class Ball extends Shape {
 
 }
 
+class EvilCircle extends Shape {
+   constructor(x, y) {
+      super(x, y, 20, 20);
+      this.color = "white";
+      this.size = 10;
+
+   }
+}
+
 const balls = [];
 
 while (balls.length < 25) {
